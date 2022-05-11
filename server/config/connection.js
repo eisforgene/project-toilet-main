f35b7894bea6c4c175992af489aa2738e1772952
eisforgene <82473195+eisforgene@users.noreply.github.com>
2022-05-11 00:46:28 -0700

    changed mongodb connection

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/toiletfinder"),
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/toiletfinder"),
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
